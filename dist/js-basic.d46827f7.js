d9f51ef88772b5cace73a6cb37dde9659a8be731
Uliana Kucherenko <ulyanakucherenko28@gmail.com>
2020-10-25 20:55:48 +0200

    added hw basisJS

--- dist/js-basic.d46827f7.js
+++ dist/js-basic.d46827f7.js
@@ -117,8 +117,117 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"index.js":[function(require,module,exports) {
+})({"js/js-basic.js":[function(require,module,exports) {
+'use strict'; //1
 
+var name = prompt('Введи своё имя :', '');
+alert("\u041F\u0440\u0438\u0432\u0435\u0442, ".concat(name, " !")); //2
+
+/*
+const thisYear = 2020;
+
+const yearBorn = Number(prompt("Введи год cвоего рождения: "," "));
+const yearsOld = thisYear - yearBorn;
+alert(`Тебе ${yearsOld} лет!`);
+*/
+//3
+
+/*
+const sideOfSquare = Number(prompt("Введите длину стороны квадрата(в см): а = ",""));
+const perimeterSquare = sideOfSquare * 4;
+alert(`Периметр квадрата равен ${perimeterSquare} см`);
+*/
+//4
+
+/*
+const pi =3.14;
+
+const radius = Number(prompt("Введите радиус окружности : r = "," "));
+const areaOfCircle = pi * radius * radius;
+alert(`Площадь окружности равна ${areaOfCircle} `);
+*/
+//5
+
+/*
+const distance = Number(prompt("Введите расстояние между городами (в км) : L = ", " "));
+const time = Number(prompt("Введите время, за которое хотите добраться (в часах) : t = "," "));
+
+const speed = distance / time;
+alert(`Нужная скорость равна ${speed} км/ч `);
+*/
+//6
+
+/*
+const rate= 0.85;
+
+const dollar = Number(prompt("Введите сумму в долларах : ", " "));
+const euro = (dollar * rate).toFixed(2);
+
+alert(`${dollar} USD =  ${ euro} EUR `);
+*/
+//7
+
+/*
+const fileSize = 820;
+const Gb = Number(prompt("Введите количество ГБ в флешке: ", " "));
+const fils = Math.floor((Gb * 1000) / fileSize);
+
+alert(` В флешке на ${Gb} Гб поместиться -  ${fils} файлов с размером по ${fileSize} Мб`);
+*/
+//8
+
+/*
+const money = Number(prompt("Введите количество денег: ", " "));
+const priceOneChocolate = Number(prompt("Введите сколько стоит одна шоколадка: ", " "));
+
+const nomberOfChocolates =((money/ priceOneChocolate)).toFixed(2);
+const balance = (money - (priceOneChocolate * nomberOfChocolates)).toFixed(2);
+
+
+console.log(` 
+            Всего денег = ${money} грн
+            1 шоколадка = ${priceOneChocolate} грн
+            Кол-во шоколадок, которые может купить = ${nomberOfChocolates} шт
+            Остаток на счету = ${balance} грн`
+            ); 
+
+alert(`Остаток на счету = ${balance} грн`);
+
+*/
+//9
+
+/*
+const nomberUser = Number(prompt("Введите трёхзначное число: ", " "));
+
+const first = Math.floor(nomberUser*0.01);
+const second = Math.floor(nomberUser*0.1) % 10;
+const third = nomberUser % 10;
+
+const result = third * 100 + second * 10 + first;
+
+alert(` ${result}`);
+*/
+//10
+// (Сумма нач. * % ставка * к-во дней начисления % / к-во дней в году) / 100.
+
+/*
+const depositAmount = Number(prompt("Введите сумму вклада в банк: ", " "));
+const percentDepositYear = 5;
+const daysOfYear = 365;
+const daysOfTwoMonth = 61;
+
+const sumInteresPercent = ( (( depositAmount * percentDepositYear * daysOfTwoMonth ) / daysOfYear) / 100 ).toFixed(2);
+
+console.log(`
+Сумму вклада в банк = ${depositAmount}
+Процентная ставка депозита ${percentDepositYear}% годовых
+Кол-во дней в году = ${daysOfYear}
+кол-во дней вклада = ${daysOfTwoMonth} дней (12месяцев)
+Сумма начисленных процентов = ${sumInteresPercent} грн `);
+
+alert(`Сумма начисленных процентов = ${sumInteresPercent} грн`);
+
+*/
 },{}],"C:/Users/Ульяна/AppData/Local/Yarn/Data/global/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -323,5 +432,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["C:/Users/Ульяна/AppData/Local/Yarn/Data/global/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
-//# sourceMappingURL=/js-module.e31bb0bc.js.map
+},{}]},{},["C:/Users/Ульяна/AppData/Local/Yarn/Data/global/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/js-basic.js"], null)
+//# sourceMappingURL=/js-basic.d46827f7.js.map
